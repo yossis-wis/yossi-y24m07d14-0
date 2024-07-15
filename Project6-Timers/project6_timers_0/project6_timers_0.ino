959f7431393b265b4453abd05edfb2a544d71bc3
Joseph M Steinberger <81210960+yossis-wis@users.noreply.github.com>
2024-07-15 14:16:35 +0300

    measure

--- Project6-Timers/project6_timers_0/project6_timers_0.ino
+++ Project6-Timers/project6_timers_0/project6_timers_0.ino
@@ -5,6 +5,7 @@ const int interruptPin = 2;  // Use pin 2 for interrupt
 
 // Variable to store the LED state
 volatile bool ledState = false;
+unsigned long myTime;
 
 void setup() {
   // Initialize the button pin as an input
@@ -14,7 +15,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);
 
   // Start serial communication at 9600 baud
-  Serial.begin(9600);
+  // Serial.begin(9600);
 
   // Attach interrupt to the interrupt pin, trigger on CHANGE
   attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, CHANGE);
@@ -22,23 +23,33 @@ void setup() {
 
 void loop() {
   // Update the LED state based on the interrupt
-  digitalWrite(ledPin, ledState ? HIGH : LOW);
+  // digitalWrite(ledPin, ledState ? HIGH : LOW);
+
+  int my Time = millis();
 
   // Serial statements to monitor the loop
-  Serial.println("In loop");
-  delay(1000);
+  // Serial.println("In loop");
+  // delay(1000);
 }
 
 // Interrupt service routine to toggle the LED state
 void handleInterrupt() {
   // Toggle the LED state
   // ledState = !ledState;
-  Serial.println("entered interrupt");
-  digitalWrite(ledPin, HIGH);  // Turn the LED on
+  // Serial.println("entered interrupt");
+  // digitalWrite(ledPin, HIGH);  // Turn the LED on
+  interruptFlag = true; // Set the flag
+  // Serial.print("Time: ");
+  
+  // Serial.println(myTime); // prints time since program started
+
   for (int i = 0; i < 10000; i++) {}
   // delay(500000);
-  digitalWrite(ledPin, LOW);
-  Serial.println("exited interrupt");
+
+  // digitalWrite(ledPin, LOW);
+
+  // Serial.println("exited interrupt");
+
   // } else {
   //   digitalWrite(ledPin, LOW);  // Turn the LED off
   // }
---
+++ Project7-Analog signals/project7_0/project7_0.ino
@@ -0,0 +1,127 @@
+// Define the pin numbers
+const int rotary0 = A0;
+int rotary1 = 0;
+void setup() {
+
+  pinMode(rotary0, INPUT);
+
+  // Start serial communication at 9600 baud
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  // Serial statements to monitor the loop
+  rotary1 = analogRead(rotary0);
+  Serial.println(millis());
+
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  Serial.println(millis());
+
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+    rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+    rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+    rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+    rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+    rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+    rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+    rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+    rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+    rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  rotary1 = analogRead(rotary0);
+  Serial.println(millis());
+
+}
+

8a176554254436148c7076b1f0d7f4a7ddab47e5
Joseph M Steinberger <81210960+yossis-wis@users.noreply.github.com>
2024-07-17 15:12:54 +0300

    finished with threading up to gui

--- Project8.1-Firmata_approach/Reacting_button_press_pymata4_using_sleep.py
+++ Project8.1-Firmata_approach/Reacting_button_press_pymata4_using_sleep.py
@@ -1,7 +1,8 @@
 import time
 import sys
-import threading
 from pymata4 import pymata4
+
+board = pymata4.Pymata4(com_port="COM4")
 
 DIGITAL_PIN = 6  # Arduino pin number
 LED_PIN = 4
@@ -13,8 +14,8 @@ CB_PIN = 1
 CB_VALUE = 2
 CB_TIME = 3
 
-def turnitoff():
-    board.digital_write(LED_PIN,0)
+
+# board.set_pin_mode_digital_output(LED_PIN)
 
 def the_callback(data):
     """
@@ -26,51 +27,38 @@ def the_callback(data):
     """
     date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data[CB_TIME]))
     print(f'Pin: {data[CB_PIN]} Value: {data[CB_VALUE]} Time Stamp: {date}')
-    board.digital_write(LED_PIN,1)
-
-    # Start the initial timer
-    timer = threading.Timer(POLL_TIME, turnitoff)
-    timer.start()
-
 
 def digital_in(my_board, pin):
     """
-     This function establishes the pin as a
-     digital input. Any changes on this pin will
-     be reported through the call back function.
+    This function establishes the pin as a
+    digital input. Any changes on this pin will
+    be reported through the call back function.
 
-     :param my_board: a pymata_express instance
-     :param pin: Arduino pin number
-     """
+    :param my_board: a pymata_express instance
+    :param pin: Arduino pin number
+    """
 
-    # set the pin mode
+    # Set the pin mode
     my_board.set_pin_mode_digital_input(pin, callback=the_callback)
 
     while True:
         try:
             # Do a read of the last value reported every 5 seconds and print it
-            # digital_read returns A tuple of last value change and the time that it occurred
             value, time_stamp = my_board.digital_read(pin)
             date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time_stamp))
-            # value
-            print(f'Polling - last value: {value} received on {date} ')
+            print(f'Polling - last value: {value} received on {date}')
             time.sleep(POLL_TIME)
         except KeyboardInterrupt:
             board.shutdown()
             sys.exit(0)
 
-
-board = pymata4.Pymata4(com_port="COM4")
-board.set_pin_mode_digital_output(LED_PIN)
+# board = pymata4.Pymata4()
+# board.digital_write(LED_PIN,1)
 
 try:
     digital_in(board, DIGITAL_PIN)
-    # Keep the main thread alive to allow timer to function
-    while True:
-        time.sleep(1)
 except KeyboardInterrupt:
-    # Shutdown board and cancel timers on interrupt
     board.shutdown()
-    for timer in timers:
-        timer.cancel()
     sys.exit(0)
+
+board.shutdown()
